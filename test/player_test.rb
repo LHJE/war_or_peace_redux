23d8ba7c2084a57cdc1141a6ea1b00eb405d7e20
LHJE <theeluke@gmail.com>
2021-01-29 12:08:09 -0700

    Refactor player_test to shorten up tests

--- test/player_test.rb
+++ test/player_test.rb
@@ -8,32 +8,22 @@ class PlayerTest < Minitest::Test
   def setup
     card_1 = Card.new(:diamond, 'Queen', 12)
     card_2 = Card.new(:heart, 'Ace', 14)
-    deck = Deck.new([card_1, card_2])
-
-    player = Player.new('Clarisa', deck)
-  end
-
-  def test_it_exists
-    card_1 = Card.new(:diamond, 'Queen', 12)
-    card_2 = Card.new(:heart, 'Ace', 14)
-    deck = Deck.new([card_1, card_2])
-    player = Player.new('Clarisa', deck)
-
-    assert_instance_of Player, player
-  end
-
-  def test_it_has_readable_attributes
-    card_1 = Card.new(:diamond, 'Queen', 12)
-    card_2 = Card.new(:heart, 'Ace', 14)
     card_3 = Card.new(:spade, 'Three', 3)
     card_4 = Card.new(:club, 'four', 4)
     card_5 = Card.new(:diamond, 'five', 5)
     card_6 = Card.new(:heart, 'six', 6)
-    deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6])
+    @deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6])
 
-    player = Player.new('Clarisa', deck)
+    @player = Player.new('Clarisa', @deck)
+  end
 
-    assert_equal 'Clarisa', player.name
-    assert_equal deck, player.deck
+  def test_it_exists
+    assert_instance_of Player, @player
+  end
+
+  def test_it_has_readable_attributes
+
+    assert_equal 'Clarisa', @player.name
+    assert_equal @deck, @player.deck
   end
 end
