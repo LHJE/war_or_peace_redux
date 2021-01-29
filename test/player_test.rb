745f3547737d9e9749654d60ec83228fbbf5b58e
LHJE <theeluke@gmail.com>
2021-01-29 12:05:16 -0700

    Add player_test file with initial tests

---
+++ test/player_test.rb
@@ -0,0 +1,39 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+class PlayerTest < Minitest::Test
+  def setup
+    card_1 = Card.new(:diamond, 'Queen', 12)
+    card_2 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card_1, card_2])
+
+    player = Player.new('Clarisa', deck)
+  end
+
+  def test_it_exists
+    card_1 = Card.new(:diamond, 'Queen', 12)
+    card_2 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card_1, card_2])
+    player = Player.new('Clarisa', deck)
+
+    assert_instance_of Player, player
+  end
+
+  def test_it_has_readable_attributes
+    card_1 = Card.new(:diamond, 'Queen', 12)
+    card_2 = Card.new(:heart, 'Ace', 14)
+    card_3 = Card.new(:spade, 'Three', 3)
+    card_4 = Card.new(:club, 'four', 4)
+    card_5 = Card.new(:diamond, 'five', 5)
+    card_6 = Card.new(:heart, 'six', 6)
+    deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6])
+
+    player = Player.new('Clarisa', deck)
+
+    assert_equal 'Clarisa', player.name
+    assert_equal deck, player.deck
+  end
+end
