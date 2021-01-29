d550d0df03efdbdc1b82e1323cd1c8ff82c69a0b
LHJE <theeluke@gmail.com>
2021-01-29 11:56:39 -0700

    Add remove_card method to deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -21,6 +21,10 @@ class Deck
   def percent_high_ranking
     ((self.high_ranking_cards.count.to_f / @cards.count.to_f) * 100).round(2)
   end
+  
+  def remove_card
+    @cards.shift
+  end
 
   def add_card(card)
     @cards << card
