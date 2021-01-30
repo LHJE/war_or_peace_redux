2fa366174fbb6b076c9969c2c050bc4b5ea45315
LHJE <theeluke@gmail.com>
2021-01-29 18:25:10 -0700

    Refactor to shorten methods up

--- lib/game.rb
+++ lib/game.rb
@@ -21,8 +21,11 @@ class Game
     p "Welcome to War! (or Peace) This game will be played with 52 cards."
     p "The players today are #{@player_1.name} and #{@player_2.name}."
     p "Type 'GO' to start the game!"
-
     user_index = user_input.downcase
+    start_the_game(user_index)
+  end
+
+  def start_the_game(user_index)
     if user_index != "go"
       p "Well now we're gonna have to start again.  This time please enter 'GO'"
       p "-----------------"
@@ -34,6 +37,11 @@ class Game
 
   def play_the_game
     turn_count = 0
+    game_loop(turn_count)
+    endgame
+  end
+
+  def game_loop(turn_count)
     until @player_1.has_lost? || @player_2.has_lost? == []
       turn_count += 1
       if turn_count == 1000001
@@ -42,6 +50,9 @@ class Game
         take_a_turn(turn_count)
       end
     end
+  end
+
+  def endgame
     if !@player_1.has_lost? && !@player_2.has_lost?
       p "---- DRAW ----"
     elsif @player_1.has_lost?
@@ -51,10 +62,15 @@ class Game
     end
   end
 
-
   def take_a_turn(turn_count)
     turn = Turn.new(@player_1, @player_2)
     victor = turn.winner
+    turn_text(turn_count, victor, turn)
+    turn.pile_cards
+    turn.award_spoils(victor)
+  end
+
+  def turn_text(turn_count, victor, turn)
     if turn.type == :mutually_assured_destruction
       p "Turn #{turn_count}: *mutually assured destruction* 6 cards removed from play"
     elsif turn.type == :war
@@ -62,8 +78,6 @@ class Game
     else
       p "Turn #{turn_count}: #{victor.name} won 2 cards"
     end
-    turn.pile_cards
-    turn.award_spoils(victor)
   end
 
 end
