7793a623cf59f53043bdd47f31816083563c1ae0
LHJE <theeluke@gmail.com>
2021-01-29 19:27:37 -0700

    Update card generator test to test cards method

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -14,4 +14,9 @@ class CardGeneratorTest < Minitest::Test
   def test_it_has_readable_attributes
     assert_equal "cards.txt", @cards.filename
   end
+
+  def test_cards
+    assert_instance_of Array, @cards.cards
+    assert_instance_of Card, @cards.cards[0]
+  end
 end
