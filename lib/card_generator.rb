e661cc629f91e6c960ab68c5503acc99ebce3c3d
LHJE <theeluke@gmail.com>
2021-01-29 19:27:48 -0700

    Add cards method to card_generator

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,9 +1,24 @@
+require './lib/card'
+
 class CardGenerator
 
-  attr_reader :filename
+  attr_reader :filename,
+              :cards
 
   def initialize(filename)
     @filename = filename
+    @cards = []
+  end
+
+  def cards
+    cards_info_array = File.readlines("./data/#{@filename}").each do |line|
+      line
+    end
+    cards_info_array.each do |card_info|
+      info_array = card_info[0..-2].split(", ")
+      @cards << Card.new(info_array[0].to_sym, info_array[1].split('')[1], info_array[2].to_i)
+    end
+    @cards
   end
 
 end
