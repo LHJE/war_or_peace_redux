ca85f6ccfc23d964ac2077608a73aa3321221de3
LHJE <theeluke@gmail.com>
2021-01-29 17:33:37 -0700

    Update game_test so cards are in a predetermined manor

--- test/game_test.rb
+++ test/game_test.rb
@@ -13,22 +13,28 @@ class GameTest < Minitest::Test
     card_1 = Card.new(:spade, '9', 9)
     card_2 = Card.new(:spade, '7', 7)
     card_3 = Card.new(:spade, 'Jack', 11)
+
     #war - loss
     card_4 = Card.new(:club, '3', 3)
     card_5 = Card.new(:heart, '8', 8)
     card_6 = Card.new(:club, '2', 2)
+
     #basic - loss
     card_7 = Card.new(:spade, '5', 5)
+
     #mutually_assured_destruction
     card_8 = Card.new(:diamond, 'King', 13)
     card_9 = Card.new(:heart, 'Queen', 12)
     card_10 = Card.new(:spade, 'Ace', 14)
+
     #war - win
     card_11 = Card.new(:club, '7', 7)
     card_12 = Card.new(:spade, '3', 3)
     card_13 = Card.new(:heart, '5', 5)
+
     #basic - win
     card_14 = Card.new(:club, '5', 5)
+
     #etc
     card_15 = Card.new(:diamond, 'Jack', 11)
     card_16 = Card.new(:club, 'Jack', 11)
@@ -48,22 +54,28 @@ class GameTest < Minitest::Test
     card_27 = Card.new(:diamond, '9', 9)
     card_28 = Card.new(:heart, '4', 4)
     card_29 = Card.new(:heart, 'Jack', 11)
+
     #war - win
     card_30 = Card.new(:diamond, '3', 3)
     card_31 = Card.new(:spade, '10', 10)
     card_32 = Card.new(:spade, '8', 8)
+
     #basic - win
     card_33 = Card.new(:heart, '6', 6)
+
     #mutually_assured_destruction
     card_34 = Card.new(:spade, 'King', 13)
     card_35 = Card.new(:heart, '9', 9)
     card_36 = Card.new(:club, 'Ace', 14)
+
     #war - loss
     card_37 = Card.new(:heart, '7', 7)
-    card_38 = Card.new(:diamond, 'Queen', 12)
+    card_38 = Card.new(:spade, '2', 2)
     card_39 = Card.new(:heart, '2', 2)
+
     #basic - loss
     card_40 = Card.new(:diamond, '2', 2)
+
     #etc
     card_41 = Card.new(:heart, 'King', 13)
     card_42 = Card.new(:spade, '4', 4)
@@ -71,7 +83,7 @@ class GameTest < Minitest::Test
     card_44 = Card.new(:club, 'King', 13)
     card_45 = Card.new(:diamond, '4', 4)
     card_46 = Card.new(:heart, '10', 10)
-    card_47 = Card.new(:spade, '2', 2)
+    card_47 = Card.new(:diamond, 'Queen', 12)
     card_48 = Card.new(:club, '6', 6)
     card_49 = Card.new(:club, 'Queen', 12)
     card_50 = Card.new(:club, '10', 10)
